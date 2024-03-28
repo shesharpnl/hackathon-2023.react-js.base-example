a970de6907119b123226526a6a87fad52491f95f
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2024-03-28 13:37:44 +0000

    chore: removed calendar

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../components/Header";
 import ContactForm from "../components/ContactForm"
 import Footer from "../components/Footer";
-import Calendar from "../components/calendar/Calendar";
 
 const Contact = () => {
     return (
@@ -12,7 +11,6 @@ const Contact = () => {
             <ContactForm />
             </main>
             <Footer />
-            <Calendar/>
         </>   
  )
 }
